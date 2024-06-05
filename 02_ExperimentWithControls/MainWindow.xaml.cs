1b34475a0aaf2c72652605a8cf55b38bf7d00b85
khg <winquake3@hanmail.net>
2024-06-05 21:17:49 +0900

    WPF 컨트롤러 작동

--- 02_ExperimentWithControls/MainWindow.xaml.cs
+++ 02_ExperimentWithControls/MainWindow.xaml.cs
@@ -44,5 +44,37 @@ namespace _02_ExperimentWithControls
         {
             number.Text = bigSlider.Value.ToString("000-000-0000");
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItems is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (readOnlyComboBox.SelectedItem is ListBoxItem  listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
+        }
     }
 }
