fbb59c7c71e481dee86bee574ec70f48d1e7b17f
khg <winquake3@hanmail.net>
2024-06-07 20:09:06 +0900

    콘솔 - 트럼프 카드 게임

---
+++ 03_PickRandomCards/CardPicker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_PickRandomCards
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++) 
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            //1~4 사이의 임의의 숫자를 뽑는다.
+            int value = random.Next(1, 5);
+
+            //1 이면 스페이드를 반환
+            if (value == 1) return "Spades";
+
+            //2 이면 하트를 반환
+            if (value == 2) return "Hearts";
+
+            //3 이면 클로버를 반환
+            if (value == 3) return "Clubs";
+
+            //일치하는 값이 없으면 다이아몬드 반환
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ 03_PickRandomCards/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _03_PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("선택할 카드 수를 입력하세요.");
+            string line  = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                // line 을 int 로 변환할 수 있으면 변환된 int 값은 numberOfCards 변수에 저장되고 이 코드 블록  실행
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                // lind 을 int로 변환할 수 없으면 이 코드 블록 실행
+                Console.WriteLine("입력한 값이  유요하지 않습니다.");
+            }
+        }
+    }
+}
