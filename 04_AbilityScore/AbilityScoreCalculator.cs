626e051a7224de1ce70d16ce54108029b1cc5f73
khg <winquake3@hanmail.net>
2024-06-11 01:53:19 +0900

    캐스팅 예제

---
+++ 04_AbilityScore/AbilityScoreCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _04_AbilityScore
+{
+    internal class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+        public void CalculateAbilityScore()
+        {
+            // 굴리기 값을 DivideBy 필드 값으로 나눈다.
+            double divided = RollResult / DivideBy;
+            //AddAmount 를 나눗셈 결과에 더한다
+            int added = AddAmount + (int)divided;
+            //결과 값이 너무 적으면 Minimum 값을 사용한다.
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+    }
+}
---
+++ 04_AbilityScore/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _04_AbilityScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true) 
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Calculated ability score: " + calculator.Score);
+                Console.WriteLine("Oress Q to quit, any other key to conttinue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+            }
+        }
+
+        private static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [ " + lastUsedValue + " ]: ");
+            string line = Console.ReadLine();
+            if (double.TryParse(line, out double value))
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine(" usting default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+        private static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [ " + lastUsedValue + " ]: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value)) 
+            {
+                Console.WriteLine(" using value " + value);
+                return value;
+            } 
+            else 
+            {
+                Console.WriteLine(" usting default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
