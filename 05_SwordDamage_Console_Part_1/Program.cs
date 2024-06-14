cf4fa5a4318ef578807482779512b7d60eded8f9
khg <winquake3@hanmail.net>
2024-06-15 00:02:05 +0900

    칼 데미지 계산을 위한 준비

---
+++ 05_SwordDamage_Console_Part_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05_SwordDamage_Console_Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. SwordDamage 와 Random 클래스의 새 인스턴스 만들기
+            //2. 콘솔에 메시지를 출력하고 사용자의 입력을 받는 코드 0~3 중 하나가 아닌 경우 프로그램 종료
+            //3. random.Next(1,7)을 3번 호출해 주사위 3개를 랜덤으로 굴리고ㅗ, 결과를 합산해서 Roll 변수에 저장
+            //4. 1이나 3을 입력하면 SetMagic(true)를 호출. 아닌 경우 SetMagic(false) 호출 key == '1'은 true를 반환 || 를 사용해서 if문 사용 안함
+            //5. 2 나 3을 입력하면 SetFlaming(true) 호출. 아닌 경우 SetFlaming(false) 호출 == 연산자와 || 사용하면 한줄 짜리 명령어 가능
+            //6. 콘솔에 결과 출력 줄바꿈은 \n
+        }
+    }
+}
