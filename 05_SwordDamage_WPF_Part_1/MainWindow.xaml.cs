c2219e773403191564099021f6ef7c273f559dc3
khg <winquake3@hanmail.net>
2024-06-16 19:49:56 +0900

    디버그 코드로 버그 찾기

--- 05_SwordDamage_WPF_Part_1/MainWindow.xaml.cs
+++ 05_SwordDamage_WPF_Part_1/MainWindow.xaml.cs
@@ -14,35 +14,60 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _05_SwordDamage_WPF_Part_1
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
+        Random random = new Random();
+        SwordDamage sd = new SwordDamage();
+
         public MainWindow()
         {
             InitializeComponent();
+            sd.SetMagic(false);
+            sd.SetFlaming(false);
+            RollDice();
+        }
+
+        private void RollDice()
+        {
+            sd.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            sd.SetFlaming(Flaming.IsChecked.Value);
+            sd.SetMagic(Magic.IsChecked.Value);
+            DisplayDamage();
+        }
+        
+        void DisplayDamage()
+        {
+            damage.Text = "Rolled " + sd.Roll + " for " + sd.Damage + " HP";
         }
 
         private void Flaming_Checked(object sender, RoutedEventArgs e)
         {
-
+            sd.SetFlaming(true);
+            DisplayDamage();
         }
 
         private void Flaming_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            sd.SetFlaming(false);
+            DisplayDamage();
         }
 
         private void Magic_Checked(object sender, RoutedEventArgs e)
         {
-
+            sd.SetMagic(true);
+            DisplayDamage();
         }
 
         private void Magic_Unchecked(object sender, RoutedEventArgs e)
         {
+            sd.SetMagic(false);
+            DisplayDamage();
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
         }
     }
 }
---
+++ 05_SwordDamage_WPF_Part_1/SwordDamage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace _05_SwordDamage_WPF_Part_1
+{
+    /// <summary>
+    /// 룰 설명
+    /// 보통 칼 공격시 : 주사위 3개 합 + 기본 데미지 3HP 더함
+    /// 불타는 칼 공격시 : 화염 데미지 2 HP
+    /// 마법 칼 공격시 : (int)(주사위 3개 합 * 1.75) + 기본 데미지 + 화염 데미지
+    /// </summary>
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"CalulateDamage finished: {Damage} (roll : {Roll})");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+            Debug.WriteLine($"SetMagic finished: {Damage} (roll : {Roll})");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+            Debug.WriteLine($"SetFlaming finished: {Damage} (roll : {Roll})");
+        }
+    }
+}
