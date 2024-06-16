3e437a0ede26da270d547764de377ac26e3d3cd8
khg <winquake3@hanmail.net>
2024-06-17 03:18:03 +0900

    갭슐화 테스트 코드

---
+++ 05_High_Low/HiLoGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _05_High_Low
+{
+    static class HiLoGame
+    {
+        public const int MAXIMUM = 10; //처음 기본금 (10달러)
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1); //현재의 임의의 수
+        private static int nextNumber = random.Next(1, MAXIMUM + 1); //다음 임의의 수
+        private static int pot = 10; //판돈
+
+        public static int GetPot() { return pot; }
+        
+        private static void Guess(bool higher) //판 돈과 임의 수와 비교해서 큰가? 작은가?
+        {
+            int nextNumber = random.Next(1, MAXIMUM + 1);
+            if ((higher && nextNumber >= currentNumber) || (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong");
+                pot--;
+            }
+            currentNumber = nextNumber;
+            Console.WriteLine($"The current number is {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (currentNumber >= half)
+            {
+                Console.WriteLine($"The current number is {currentNumber}, the next number is at least {half}");
+            }
+            else
+            {
+                Console.WriteLine($"The current number is {currentNumber}, the next is at most {half}");
+            }
+            pot--;
+        }
+    }
+}
---
+++ 05_High_Low/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _05_High_Low
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HiLo");
+            Console.WriteLine($"Guess  numbers between 1 and {HiLoGame.MAXIMUM}.");
+            HiLoGame.Hint();
+            while (HiLoGame.GetPot() > 0)
+            {
+                Console.WriteLine("Press h for higher, 1 for lower, ? tto buy a hint, ");
+                Console.WriteLine($"or any other key to quit with {HiLoGame.GetPot()}.");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == 'h') HiLoGame.Guess(true);
+                else if (key == '1') HiLoGame.Guess(false);
+                else if (key == '?') HiLoGame.Hint();
+                else return;
+            }
+            Console.WriteLine("The pot is empty. Bye!");
+
+        }
+    }
+}
