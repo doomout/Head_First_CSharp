64931a4c359a2856edb714c9dbaaab44e562f4d1
khg <winquake3@hanmail.net>
2024-06-19 02:18:30 +0900

    데미지 계산기 콘솔 : 캡슐화로 변경

--- 05_SwordDamage_Console_Part_1/Program.cs
+++ 05_SwordDamage_Console_Part_1/Program.cs
@@ -4,32 +4,27 @@ namespace _05_SwordDamage_Console_Part_1
 {
     internal class Program
     {
+        static Random random = new Random();
         static void Main(string[] args)
         {
-            //1. SwordDamage 와 Random 클래스의 새 인스턴스 만들기
-            SwordDamage sd = new SwordDamage();
-            Random random = new Random();
+            SwordDamage sd = new SwordDamage(RollDice());
+           
 
             while (true)
             {
-                //2. 콘솔에 메시지를 출력하고 사용자의 입력을 받는 코드 0~3 중 하나가 아닌 경우 프로그램 종료
                 Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit : ");
                 char key = Console.ReadKey().KeyChar;
                 if (!(key == '0' || key == '1' || key == '2' || key == '3')) return;
-
-                //3. random.Next(1,7)을 3번 호출해 주사위 3개를 랜덤으로 굴리고, 결과를 합산해서 roll 변수에 저장
-                int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
-                sd.Roll = roll;
-
-                //4. 1이나 3을 입력하면 SetMagic(true)를 호출. 아닌 경우 SetMagic(false) 호출 key == '1'은 true를 반환 || 를 사용해서 if문 사용 안함
-                sd.SetMagic(key == '1' || key == '3');
-
-                //5. 2 나 3을 입력하면 SetFlaming(true) 호출. 아닌 경우 SetFlaming(false) 호출 == 연산자와 || 사용하면 한줄 짜리 명령어 가능
-                sd.SetFlaming(key == '2' || key == '3');
-
-                //6. 콘솔에 결과 출력 줄바꿈은 \n
-                Console.WriteLine("\nRolled " + roll + " for " + sd.Damage + " HP\n");
+                sd.Roll = RollDice();
+                sd.Magic = (key == '1' || key == '3');
+                sd.Flaming = (key == '2' || key == '3'); 
+                Console.WriteLine($"\nRolled { sd.Roll}  for  { sd.Damage} HP\n");
             }
+        }
+
+        private static int RollDice()
+        {
+            return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
         }
     }
 }
--- 05_SwordDamage_Console_Part_1/SwordDamage.cs
+++ 05_SwordDamage_Console_Part_1/SwordDamage.cs
@@ -8,19 +8,90 @@ namespace _05_SwordDamage_Console_Part_1
 {
     internal class SwordDamage
     {
-        public const int BASE_DAMAGE = 3;
-        public const int FLAME_DAMAGE = 2;
+        private const int BASE_DAMAGE = 3;
+       private const int FLAME_DAMAGE = 2;
 
-        public int Roll;
-        public decimal MagicMultiplier = 1M;
-        public int FlamingDamage = 0;
-        public int Damage;
-
-        public void CalculateDamage()
+        /// <summary>
+        /// 생성자는 기본 Magic, Flaming 값과 주사위 3개를 굴려서 나온 값을 기준으로 데미지를 계산
+        /// </summary>
+        /// <param name="startingRoll">주사위 3개를 굴려서 나오는 값</param>
+        public SwordDamage(int startingRoll)
         {
-            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            roll = startingRoll;
+            CalculateDamage();
         }
 
+
+        /// <summary>
+        /// 주사위 3개를 굴려서 나온 값을 설정하거나 반환
+        /// </summary>
+        private int roll;
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+     
+
+        /// <summary>
+        /// 계산된 데미지 값을 저장
+        /// </summary>
+        public int Damage { get; private set; }
+
+        private bool magic;
+        /// <summary>
+        /// 마법칼이면 true, 아니면 false 반환
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// 불타는 칼이면 true, 아니면 false 반환
+        /// </summary>
+        private bool flaming;
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// 현재 속성들을 기준으로 데미지를 계산한다.
+        /// </summary>
+        //public decimal MagicMultiplier = 1M; //기존 코드
+        //public int FlamingDamage = 0; //기존 코드
+        public void CalculateDamage()
+        {
+            //Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage; //기존 코드
+            decimal magicMultiplier = 1M;
+            if (Magic) 
+                magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+
+            if (Flaming) 
+                Damage += FLAME_DAMAGE;
+        }
+
+        
+        
+        /** 기존 코드
         public void SetMagic(bool isMagic)
         {
             if (isMagic)
@@ -42,5 +113,6 @@ namespace _05_SwordDamage_Console_Part_1
                 Damage += FLAME_DAMAGE;
             }
         }
+        */
     }
 }
