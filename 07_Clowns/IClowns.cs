ce17838fbdb924845cb7dfb4967541e3c5f3f2f8
khg <winquake3@hanmail.net>
2024-06-29 21:13:58 +0900

    주석 추가

--- 07_Clowns/IClowns.cs
+++ 07_Clowns/IClowns.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace _07_Clowns
 {
+    // IClowns 인터페이스는 FunnyThingIHave 프로퍼티와 Honk 메서드를 정의합니다.
     internal interface IClowns
     {
         string FunnyThingIHave { get; }
--- 07_Clowns/TallGuy.cs
+++ 07_Clowns/TallGuy.cs
@@ -4,20 +4,26 @@ using System.Text;
 
 namespace _07_Clowns
 {
+    // TallGuy 클래스는 IClowns 인터페이스를 구현합니다.
     internal class TallGuy : IClowns
     {
+        // 이름과 키를 나타내는 필드입니다.
         public string Name;
         public int Height;
 
+        // FunnyThingIHave 프로퍼티는 "big shoes"를 반환합니다.
         public string FunnyThingIHave
         {
             get { return "big shoes"; }
         }
 
+        // Honk 메서드는 콘솔에 "Honk honk!"를 출력합니다.
         public void Honk()
         {
             Console.WriteLine("Honk honk!");
         }
+
+        // TalkAboutYourself 메서드는 자신의 이름과 키를 콘솔에 출력합니다.
         public void TalkAboutYourself()
         {
             Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
