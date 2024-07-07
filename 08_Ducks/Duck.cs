d303b5024a6cc616bf5e386200e7794658ab4de2
khg <winquake3@hanmail.net>
2024-07-07 21:15:29 +0900

    리스트 초기화 예제

---
+++ 08_Ducks/Duck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace _08_Ducks
+{
+    internal class Duck
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind {  get; set; }
+    }
+}
---
+++ 08_Ducks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08_Ducks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 17},
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 18},
+                new Duck() { Kind = KindOfDuck.Loon, Size = 14},
+                new Duck() { Kind = KindOfDuck.Muscovy, Size = 11},
+                new Duck() { Kind = KindOfDuck.Mallard, Size = 14},
+                new Duck() { Kind = KindOfDuck.Loon, Size = 13},
+            };
+            PrintDucks(ducks);
+        }
+
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine($"{duck.Size} inch {duck.Kind}");
+            }
+        }
+    }
+}
