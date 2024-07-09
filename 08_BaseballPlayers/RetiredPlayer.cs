6a111ad0e7960fc9db28048a09fb50a03b1f6fa9
khg <winquake3@hanmail.net>
2024-07-09 23:36:16 +0900

    Dictionary 예제

---
+++ 08_BaseballPlayers/RetiredPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08_BaseballPlayers
+{
+    internal class RetiredPlayer
+    {
+        public string Name { get; private set; }
+        public int YearRetired { get; private set; }
+        public RetiredPlayer(string player, int yearRetired)
+        {
+            Name = player;
+            YearRetired = yearRetired;
+        }
+    }
+}
