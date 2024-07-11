7dec786a96e92e0c2f9c0569d83737d2ba4222eb
khg <winquake3@hanmail.net>
2024-07-12 06:33:30 +0900

    Linq예제

---
+++ 09_JimmyLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_JimmyLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IEnumerable<Comic> mostExpensive = //Comic를 반환했었음
+            var mostExpensive =
+                from comic in Comic.Catalog   
+                where Comic.Prices[comic.Issue] > 500 //500달러 이상
+                orderby Comic.Prices[comic.Issue] descending //내림차순으로 정렬
+                select comic;
+           
+            foreach (Comic comic in mostExpensive)
+            {
+                Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
+            }
+        }
+    }
+}
---
+++ 09_LinqTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _09_LinqTest
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 100; i++)
+                numbers.Add(i);
+
+            //Take(5) : 시작 부분에 있는 요소를 5개 가져옴 1,2,3,4,5
+            //TakeLast(5) : 끝 부분에 있는 요소를 5개 가져옴 96,97,98,99,100
+            //Concat() 두 데이터를 결합함 1,2,3,4,5,96,97,98,99,100
+            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+            foreach (int i in firstAndLastFive)
+            {
+                Console.Write($"{i}, ");
+            }
+        }
+    }
+}
