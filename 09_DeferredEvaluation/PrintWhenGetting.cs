08b9068c8707a63d501bc8968eb0e0c715e2bf03
khg <winquake3@hanmail.net>
2024-07-13 21:50:45 +0900

    Linq 예제

---
+++ 09_DeferredEvaluation/PrintWhenGetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_DeferredEvaluation
+{
+    internal class PrintWhenGetting
+    {
+        private int instanceNumber;
+        public int InstanceNumber
+        {
+            set { instanceNumber = value; }
+            get {
+                Console.WriteLine($"Getting #{instanceNumber}");
+                return instanceNumber; 
+            }
+        }
+    }
+}
---
+++ 09_DeferredEvaluation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09_DeferredEvaluation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfObjects = new List<PrintWhenGetting>();
+            for (int i = 1; i < 5; i++)
+                listOfObjects.Add(new PrintWhenGetting() { InstanceNumber = i });
+            Console.WriteLine("Set up the query");
+            var result = 
+                from o in listOfObjects
+                select o.InstanceNumber;
+
+            var immediate = result.ToList(); 
+            Console.WriteLine("Run the foreach");
+            foreach(var number in  immediate)
+                Console.WriteLine($"Writing #{number}");
+        }
+    }
+}
