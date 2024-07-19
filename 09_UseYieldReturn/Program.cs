b26d4617136d59eea774659a7c78666910f0e4e4
khg <winquake3@hanmail.net>
2024-07-19 23:27:36 +0900

    yield 예제

---
+++ 09_UseYieldReturn/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09_UseYieldReturn
+{
+    internal class Program
+    {
+        static IEnumerable<string> SimpleEnumerable()
+        {
+            yield return "apples";
+            yield return "oranges";
+            yield return "bananas";
+            yield return "unicorns";
+        }
+        static void Main(string[] args)
+        {
+           foreach (var item in SimpleEnumerable()) 
+                Console.WriteLine(item);
+        }
+    }
+}
