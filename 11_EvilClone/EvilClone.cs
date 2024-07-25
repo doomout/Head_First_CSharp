a26f9794131b5fd69359a535d2c22c97bc10f8c5
khg <winquake3@hanmail.net>
2024-07-25 21:57:09 +0900

    종료자 예제

---
+++ 11_EvilClone/EvilClone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_EvilClone
+{
+    internal class EvilClone
+    {
+        public static int CloneCount = 0;
+        public int CloneID { get; } = ++CloneCount;
+        public EvilClone() => Console.WriteLine("Clone #{0} is wreaking havoc", CloneID);
+        ~EvilClone() //종료자
+        {
+            Console.WriteLine("Clone #{0} destroyed", CloneID);        
+        }
+    }
+}
---
+++ 11_EvilClone/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11_EvilClone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var clones = new List<EvilClone>();
+            while (true)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'a': //a를 누르면 객체 생성
+                        clones.Add(new EvilClone()); 
+                        break;
+                    case 'c': //c를 누르면 모든 참조 제거
+                        Console.WriteLine("Clearing list at time {0}", stopwatch.ElapsedMilliseconds);
+                        clones.Clear();
+                        break;
+                    case 'g': //g를 누르면 CLR이 가비지 컬렉션 대상으로 표시된 모든 객체를 수집
+                        Console.WriteLine("Collection at time {0}", stopwatch.ElapsedMilliseconds);
+                        GC.Collect();
+                        break;
+                }
+            }
+        }
+    }
+}
