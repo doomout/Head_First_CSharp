beb7a7e40ec9ca6af2b5f70b964f3543e44a68d4
khg <winquake3@hanmail.net>
2024-07-28 17:15:17 +0900

    ref 한정자를 사용해 참조로 전달하기

---
+++ 11_PassGuyByReference/Guy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_PassGuyByReference
+{
+    internal class Guy
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"a {Age}-year-old named {Name}";
+        }
+    }
+}
---
+++ 11_PassGuyByReference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _11_PassGuyByReference
+{
+    internal class Program
+    {
+        //ref : 메서드에 참조로 인수를 전달하는 방식
+        static void ModifyAnIntAndGuy(ref int valueRef, ref Guy guyRef)
+        {
+            valueRef += 10;
+            guyRef.Name = "Bob";
+            guyRef.Age = 37;
+        }
+        static void Main(string[] args)
+        {
+            var i = 1;
+            var guy = new Guy() { Name = "Joe", Age = 26 };
+            Console.WriteLine("i is {0} and guy is {1}", i, guy);
+            ModifyAnIntAndGuy(ref i, ref guy); //a {Age}-year-old named {Name}
+            Console.WriteLine("Now i is {0} and guy is {1}", i, guy);
+        }
+    }
+}
+/**실행 결과
+i is 1 and guy is a 26-year-old named Joe
+Now i is 11 and guy is a 37-year-old named Bob
+ */
