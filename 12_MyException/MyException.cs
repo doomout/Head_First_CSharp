c7108627aa64ac3aa2b7791132972f9973b8dd00
khg <winquake3@hanmail.net>
2024-07-30 12:36:13 +0900

    사용자 정의 예외처리

---
+++ 12_MyException/MyException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12_MyException
+{
+    internal class MyException : Exception{ }
+
+    class ExTestDrive
+    {
+        public static void Zero(string test)
+        {
+            try
+            {
+                Console.Write("t");
+                DoRisky(test);
+                Console.Write("o");
+            }
+            catch (MyException) //사용자 정의 예외 처리
+            {
+                Console.Write("a");
+            }
+            finally
+            {
+                Console.Write("w");
+            }
+            Console.Write("s");
+        }
+
+        private static void DoRisky(string t)
+        {
+            Console.Write("h");
+            if(t == "yes")
+            {
+                throw new MyException();
+            }
+            Console.Write("r");
+        }
+    }
+}
---
+++ 12_MyException/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _12_MyException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("when it ");
+            ExTestDrive.Zero("yes");
+            Console.Write(" it ");
+            ExTestDrive.Zero("no");
+            Console.Write(".");
+        }
+    }
+}
+//실행결과
+//when it thaws it throws.
