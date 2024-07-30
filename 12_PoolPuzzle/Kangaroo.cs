7a4a71392d6e7434e42fe08331fc94f0843d8c28
khg <winquake3@hanmail.net>
2024-07-30 10:06:47 +0900

    예외 처리 예제

---
+++ 12_PoolPuzzle/Kangaroo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace _12_PoolPuzzle
+{
+    internal class Kangaroo
+    {
+        FileStream fs;
+        int croc;
+        int dingo = 0;
+        public int Wombat(int wallaby)
+        {
+            dingo++;
+            try
+            {
+                if(wallaby > 0)
+                {
+                    fs = File.OpenWrite("wobbiegong");
+                    croc = 0;
+                } 
+                else if(wallaby < 0)
+                {
+                    croc = 3;
+                }
+                else
+                {
+                    fs = File.OpenRead("wobbiegong");
+                    croc = 1;
+                }
+            }
+            catch (IOException)
+            {
+                croc = -3;
+            }
+            catch
+            {
+                croc = 4;
+            }
+            finally
+            {
+                if(dingo > 2)
+                {
+                    croc -= dingo;
+                }
+            }
+            return croc;
+        }
+    }
+}
---
+++ 12_PoolPuzzle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _12_PoolPuzzle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kangaroo joey = new Kangaroo();
+            int koala = joey.Wombat(joey.Wombat(joey.Wombat(1))); //0을 만듬
+            try
+            {
+                Console.WriteLine((15 / koala) + " eggs per pound");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("G'Day Mate!");
+            }
+        }
+    }
+}
+
+/**실행 결과
+ G'Day Mate!
+ */
