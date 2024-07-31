2a3de6e65f8bac98a830e9aac8deec3fbbfb3898
khg <winquake3@hanmail.net>
2024-08-01 00:07:55 +0900

    웹 컨트롤러 예제

---
+++ 13_ControlsBlazor/Program.cs
@@ -0,0 +1,11 @@
+using _13_ControlsBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
