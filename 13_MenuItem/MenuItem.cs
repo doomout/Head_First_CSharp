e230c8e65f3e19ebe448ec87c2e493420bc77a7d
khg <winquake3@hanmail.net>
2024-08-02 04:03:22 +0900

    랜덤 메뉴 웹버전

---
+++ 13_MenuItem/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿namespace _13_MenuItem
+{
+    public class MenuItem
+    {
+        public static Random Randmizer = new Random();
+        public string[] Proteins = { "구운 소고기", "살라미", "칠면조", "햄", "파스트라미", "두부" };
+        public string[] Condiments = { "노란 머스타드", "갈색 머스타드", "허니 머스타드", "마요네즈", "케챱", "프렌치 드레싱" };
+        public string[] Breads = { "호밀", "쌀", "밀", "호밀 가루", "롤" };
+
+        public string Description = "";
+        public string Price = "";
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randmizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randmizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randmizer.Next(Breads.Length)];
+            Description = randomProtein + " + " + randomCondiment + " + " + randomBread;
+
+            decimal bucks = Randmizer.Next(2, 5);
+            decimal cents = Randmizer.Next(1, 98);
+            decimal price = bucks + (cents * 0.01M);
+            Price = price.ToString("C2", new System.Globalization.CultureInfo("en-US"));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -18,9 +18,12 @@ IDE : Microsoft Visual Studio Community 2022
 ### 5. 이모지 매칭 게임 웹 버전(Blazor WebAssembly)
 ![Cap 2024-07-31 12-57-15-104](https://github.com/user-attachments/assets/cc20b69e-6a54-4080-9794-3542653635da)
 
-### 6. 웹 컨트롤러 예제
+### 6. 웹 컨트롤러 예제(Blazor WebAssembly)
 ![Cap 2024-07-31 17-45-06-358](https://github.com/user-attachments/assets/63f71dcb-e144-46c4-b83c-d78084310aad)
 
 ### 7. 랜덤 카드 뽑기 웹 버전(Blazor WebAssembly)
 ![Cap 2024-08-01 00-29-55-766](https://github.com/user-attachments/assets/2ccb6385-4f93-4212-854b-229f015798f9)
+
+### 8. 랜덤 메뉴 웹 버전(Blazor WebAssembly)
+![Cap 2024-08-01 08-17-00-967](https://github.com/user-attachments/assets/3b55dc9d-4997-4129-a20f-c0eba9654f8b)
 
